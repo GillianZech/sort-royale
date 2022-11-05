abca6a77d3e0c3ad2f4c1076964a5e65d155cc28
Simi Singh <34402769+archeopsor@users.noreply.github.com>
2022-11-05 13:23:38 -0400

    tutorial

--- src/components/NumberList.js
+++ src/components/NumberList.js
@@ -5,12 +5,10 @@ class NumberList extends Component {
         return (
             <div>
                 <ul>
-                    <li>1</li>
-                    <li>2</li>
-                    <li>1</li>
+                    <li>5</li>
                     <li>4</li>
-                    <li>7</li>
-                    <li>6</li>
+                    <li>3</li>
+                    <li>1</li>
                     <li>2</li>
                 </ul>
             </div>
---
+++ src/components/tutorials/InsertionSort.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class InsertionSort extends Component {
+    
+
+    render () {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default InsertionSort;
