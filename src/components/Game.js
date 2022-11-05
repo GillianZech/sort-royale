277462dad142887ae32f98821e0fe4e2fab57c1a
Simi Singh <34402769+archeopsor@users.noreply.github.com>
2022-11-05 13:15:35 -0400

    some game setup stuff

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import NumberList from './NumberList.js'
 
 class Game extends Component {
+    setCurrentNumbers = (left, right) => {
+        this.setState({
+            left_number: left,
+            right_number: right
+        })
+    }
+
     render () {
         return (
             <div className="Game">
-                <p>Test</p>
+                <div className="number-list">
+                    <p>Numbers</p>
+                    <NumberList />
+                </div>
+                <button className="left-num">{this.state.left_number}</button>
+                <button className="right-num">{this.state.right_number}</button>
             </div>
         )
     }
+
+    constructor() {
+        super();
+    
+        this.state = {
+          number_list: [],
+          left_number: "",
+          right_number: "",
+        }
+      };
 }
 
 export default Game;
---
+++ src/components/NumberList.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class NumberList extends Component {
+    render () {
+        return (
+            <div>
+                <ul>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>1</li>
+                    <li>4</li>
+                    <li>7</li>
+                    <li>6</li>
+                    <li>2</li>
+                </ul>
+            </div>
+            )
+    }
+}
+
+export default NumberList;
