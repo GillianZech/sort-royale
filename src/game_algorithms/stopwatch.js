9cc0ecb996d22f840683b688d85ed096ae1103b7
Simi Singh <34402769+archeopsor@users.noreply.github.com>
2022-12-11 15:15:59 -0500

    Fix stopwatch being slow

--- src/game_algorithms/stopwatch.js
+++ src/game_algorithms/stopwatch.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 const Stopwatch = (glob) => {
+    const start_time = new Date();
     const [time, setTime] = useState(0);
     const [running, setRunning] = useState(true);
     useEffect(() => {
@@ -8,20 +9,22 @@ const Stopwatch = (glob) => {
       if (running) {
         setRunning(true);
         interval = setInterval(() => {
-          setTime((prevTime) => prevTime + 10);
+          let new_time = new Date();
+          setTime(new_time - start_time);
         }, 10);
       } else if (!running) {
         setRunning(false);
         clearInterval(interval);
       }
       return () => clearInterval(interval);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [running]);
     return (
         <div className="stopwatch">
           <div className="numbers">
             <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
             <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
-            <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+            <span>{("0" + Math.floor(((time / 10) % 100))).slice(-2)}</span>
           </div>
           {() => (glob[0]=time)}
         </div>
